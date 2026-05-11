6409b316a3847c40256c8fa92a17ab40b9f646fb
TekiSathwika <24B11DS203@adityauniversity.in>
2026-05-11 21:43:45 +0530

    Task4.py

--- Basic-programs-of-python/Task4.py
+++ Basic-programs-of-python/Task4.py
@@ -1,4 +1,5 @@
-#print even numbers from 1 to 20
-for i in range(1, 21):
+# Print even numbers up to user input
+n = int(input("Enter the limit: "))
+for i in range(1, n + 1):
     if i % 2 == 0:
         print(i)
