79ba41185595a79ffcd83c8de987e46466bbd8e6
TekiSathwika <24B11DS203@adityauniversity.in>
2026-05-14 18:57:31 +0530

    task-2.py

---
+++ control flow example programs/task-2.py
@@ -0,0 +1,9 @@
+#2. AI Chatbot Login System
+#Task: Create a simple login system.
+
+Username = input("Enter username: ")
+Password = input("Enter password: ")
+if Username == "admin" and Password == "1234":
+    print("Login Successful")
+else:
+    print("Invalid Credentials")
