90efae869b984c769c50eb54584ae2218a415a8e
TekiSathwika <24B11DS203@adityauniversity.in>
2026-05-17 16:48:34 +0530

    program-3.py

---
+++ loops&strings example programs/program-3.py
@@ -0,0 +1,5 @@
+#3.Write a Python program to print the multiplication table of a number entered by the user
+
+num= int(input("enter the number:"))
+for i in range(1,11):
+    print(num,"x",i,"=",num*i)
