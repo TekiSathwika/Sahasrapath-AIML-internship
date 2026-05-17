2b10c007eccb505127f703188048d90a0602b64a
TekiSathwika <24B11DS203@adityauniversity.in>
2026-05-17 17:19:58 +0530

    Delete loops&strings example programs/program-10.py

--- loops&strings example programs/program-10.py
+++
@@ -1,7 +0,0 @@
-#10.Write a Python program to print a star pattern of 5 rows.
-
-rows = int(input("enter the number of rows:"))
-for i in range(1, rows + 1):
-    for j in range(1, i + 1):
-        print("*", end="")
-    print() 
