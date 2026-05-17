843705fe26e8b2c98f967d2c84c9b8ac58cf4cd2
TekiSathwika <24B11DS203@adityauniversity.in>
2026-05-17 17:33:15 +0530

    program-19.py

---
+++ loops&strings example programs/program-19.py
@@ -0,0 +1,8 @@
+#19.Write a Python program to take student details and display them using string formatting.
+
+name = input("Enter name: ")
+course = input("Enter course: ")
+marks = input("Enter marks: ")
+print(f"Student Name: {name}")
+print(f"Course: {course}")
+print(f"Marks: {marks}")
