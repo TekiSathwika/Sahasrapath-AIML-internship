694f2a538ea8d6fbe63bea384c4f4b323d46c6fa
TekiSathwika <24B11DS203@adityauniversity.in>
2026-05-17 16:49:17 +0530

    program-4.py

---
+++ loops&strings example programs/program-4.py
@@ -0,0 +1,8 @@
+#4.Write a Python program to find the sum of first N natural numbers
+
+num = int(input("enter number"))
+temp=0
+for i in range(1,num+1):
+    temp = temp+i
+print("sum =",temp)
+    
